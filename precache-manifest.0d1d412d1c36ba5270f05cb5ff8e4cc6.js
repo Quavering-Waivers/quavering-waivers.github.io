09b9bc1d5ad36eab720b819d776b056cee58bcab
sambodjango <gringolalia@gmail.com>
2021-01-28 13:32:07 -0300

    deploy

---
+++ precache-manifest.0d1d412d1c36ba5270f05cb5ff8e4cc6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e506bc5beddd72504a6",
+    "url": "/css/app.58ba00b6.css"
+  },
+  {
+    "revision": "6472d8dc0446798e46f2",
+    "url": "/css/chunk-vendors.6b88b602.css"
+  },
+  {
+    "revision": "39083cdef834fe0d7ee39e0272d1aa76",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.39083cde.woff"
+  },
+  {
+    "revision": "d960b795b343e35150c1a0aa3c38c16b",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.d960b795.woff2"
+  },
+  {
+    "revision": "7af3ec51606cced202a1ce67405c7a47",
+    "url": "/img/alchemy-sulphuric.7af3ec51.svg"
+  },
+  {
+    "revision": "8c4120b4e88276badbc808e518572fad",
+    "url": "/img/logo.8c4120b4.png"
+  },
+  {
+    "revision": "5333754f3dbddd51b4d9e9fc71c9e482",
+    "url": "/img/nyc-dynamo.5333754f.jpg"
+  },
+  {
+    "revision": "c27f910c6906de7f366a91eb70021aed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94ed74d0b649fd680624",
+    "url": "/js/about.6da7340a.js"
+  },
+  {
+    "revision": "7e506bc5beddd72504a6",
+    "url": "/js/app.dd0ee401.js"
+  },
+  {
+    "revision": "6472d8dc0446798e46f2",
+    "url": "/js/chunk-vendors.4da033b8.js"
+  },
+  {
+    "revision": "2d443a851ebeac3e1e89e07c19907174",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
